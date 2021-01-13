8f5fb29bec0aac2c0944d155c521be92d622610a
XIAOLONG LI <1491skdpf89@gmail.com>
2021-01-13 14:24:35 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
+
+// 1. Test click each one get the innerHTML of that section.
+// const sections = document.querySelectorAll('section');
+// for(let i = 0; i < sections.length; i++) {
+//     sections[i].addEventListener('click', function(){
+//         var pageNav = sections[i].getAttribute("data-nav");
+//         alert('The ' + sections[i].innerHTML + " a " + pageNav);
+//     });
+// }
+
 /**
  * 
  * Manipulating the DOM exercise.
@@ -22,7 +32,7 @@ const sectionsList = document.querySelectorAll('section');
 // console.log(sectionsList);
 const frag = document.createDocumentFragment();
 const navBar = document.getElementById('navbar__list');
-// const listItems = document.getElementsByTagName('li');
+const listItems = document.getElementsByTagName('li');
 
 
 window.addEventListener('DOMContentLoaded', (event) => {
@@ -41,11 +51,6 @@ function createNavList(id, name) {
 }
 function nearPortView(element) {
     const size = element.getBoundingClientRect();
-    // console.log("1. top "+size.top);
-    // console.log("2. left "+size.left);
-    // console.log("3. bottom "+size.bottom);
-    // console.log("4. right "+size.right);
-    // console.log('--------------------');
     return (size.top >= 0 && 
         size.left >= 0 && 
         size.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
@@ -64,7 +69,6 @@ function buildNav() {
         const section_dataNav = section.getAttribute('data-nav');
         list.innerHTML = createNavList(section_id, section_dataNav);
         frag.appendChild(list);
-        
     }
     navBar.appendChild(frag);
 }
@@ -74,10 +78,12 @@ function setActive() {
     for(const section of sectionsList) {
         if(nearPortView(section)) {
             section.classList.add("your-active-class");
+            document.querySelector(`a#{section.id}`).classList.add("your-active-class");
             //console.log("the class was added!");
         }
         else {
             section.classList.remove("your-active-class");
+            document.querySelector(`a#{section.id}`).classList.remove("your-active-class");
             //console.log("the class was removed!");
         }
     }
@@ -85,8 +91,7 @@ function setActive() {
 
 // Scroll to anchor ID using scrollTO event
 function scrollAnchor(event) {
-    for(const section in sectionsList){
-        //console.log('section id'+section.id);
+    for(const section of sectionsList){
         if(event.target.nodeName === 'A') {
             const id = event.target.getAttribute('id');
             if(id === section.id) {
@@ -102,39 +107,24 @@ function scrollAnchor(event) {
 */
 
 // Build menu 
+buildNav();
 
-// Scroll to section on link click
-
+// Set sections as active
 document.addEventListener('scroll', function() {
     setActive();
 });
 
-// Set sections as active
+// Scroll to section on link click
 const navList = document.getElementById('navbar__list');
-//console.log(navList);
+console.log(navList);
 navList.addEventListener('click', function(event) {
     scrollAnchor(event);
 });
 
-buildNav(sectionsList);
 
-
-// scroll to top button
+// Addtion: scroll to top button
 const ele = document.getElementById("main");
-console.log(ele);
+// console.log(ele);
 function scrollToTop() {
     ele.scrollIntoView({behavior: "smooth"});
 }
-
-
-
-/* note - no need*/
-
-// 1. Test click each one get the innerHTML of that section.
-// const sections = document.querySelectorAll('section');
-// for(let i = 0; i < sections.length; i++) {
-//     sections[i].addEventListener('click', function(){
-//         var pageNav = sections[i].getAttribute("data-nav");
-//         alert('The ' + sections[i].innerHTML + " a " + pageNav);
-//     });
-// }
